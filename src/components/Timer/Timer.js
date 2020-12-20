39b9d8059a302867d04cf02f1e667ea42e9c3c64
Farid Huseynov <faridhuseynov.eng@gmail.com>
2020-12-21 01:13:12 +0400

    Timer properly works

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,41 +1,68 @@
-import React, { Component } from "react"
+import React from "react"
 
-class Timer extends Component {
-    state = {
-        // processStatus: props.timerProcessStatus,
-        proccessStatus:true,
-        time: 25,
-        timerStatus: "Session"
-    }
-
-
-    countDown() {
-        this.setState(prevValue=>({
-            time: prevValue - 1
-        }))
-    }
-    render() {
-        {
-            if(this.state.proccessStatus){
-                setTimeout(() => {
-                    this.countDown();
-                }, 1000);
-            }
-        }
-        return (
-            <div>
-                <p id="timer-label">{this.state.timerStatus}</p>
-                <p id="time-left">{this.state.time}</p>
-            </div>
-        )
-    }
+const timer = (props) => {
+    return (
+        <div>
+            <p id="timer-label">{props.timerStatus}</p>
+            <p id="time-left">{props.sessionLengthMinutes} : {props.sessionLengthSeconds}</p>
+        </div>
+    )
 }
 
+export default timer;
 
-export default Timer;
-// {if(this.state.processStatus === true){
-//     setTimeout(() => {
-//         console.log(processStatus);
-//         this.countDown();
-//     }, 1000)
-// }}
+// import React, { Component } from "react"
+
+// class Timer extends Component {
+//     constructor(props){
+//         super(props)
+//     };
+//     // state = {
+//     //     // processStatus: props.timerProcessStatus,
+//     //     proccessStatus:this.props.timerProcessStatus,
+//     //     minutes: this.props.sessionLength,
+//     //     seconds:0,
+//     //     timerStatus: "Session"
+//     // }
+
+//     shouldComponentUpdate(){
+//         console.log(this.props.timerProcessStatus);
+//         if(this.props.timerProcessStatus){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+
+//     countDown() {
+//         this.setState(prevState=>({
+//             minutes: prevState.minutes - 1
+//         }))
+//     }
+//     render() {
+//         {
+//             console.log(this.props.timerProcessStatus);
+//             // if(this.state.proccessStatus){
+//             //     setTimeout(() => {
+//             //         this.countDown();
+
+//             //     }, 2000);
+//             // }
+//         }
+//         return (
+//             <div>
+//                 <p id="timer-label">{this.state.timerStatus}</p>
+//                 <p id="time-left">{this.state.minutes} : {this.state.seconds}</p>
+//             </div>
+//         )
+//     }
+// }
+
+
+// export default Timer;
+// // {if(this.state.processStatus === true){
+// //     setTimeout(() => {
+// //         console.log(processStatus);
+// //         this.countDown();
+// //     }, 1000)
+// // }}
--- src/containers/Display/Display.js
+++ src/containers/Display/Display.js
@@ -6,11 +6,17 @@ class Display extends Component {
 
     state = {
         sessionLength: 25,
+        minutes:"25",
+        seconds:"00",
         breakLength: 5,
         timerStarted:false,
+        intervalId:"", // this is required to get the setInterval later
         timerStatus:"Session"
     }
 
+    componentWillUnmount(){
+        clearInterval(this.state.intervalId);
+    }
     incrementHandler = (event) => {
        let id = event.target.parentElement.previousSibling.id;
         if(id==='break-length' && this.state.breakLength<61){
@@ -21,10 +27,11 @@ class Display extends Component {
                 // console.log(this.state.breakLength);
             })
         }
-        else if(id==='session-length' && this.state.sessionLength<61){
+        else if(id==='session-length' && this.state.sessionLength<60){
             this.setState((prevState) => ({
                 ...prevState,
-                sessionLength: prevState.sessionLength + 1
+                sessionLength: prevState.sessionLength + 1,
+                minutes:prevState.minutes+1
             }), () => {
                 // console.log(this.state.sessionLength);
             })
@@ -44,31 +51,68 @@ class Display extends Component {
          else if(id==='session-length' && this.state.sessionLength>1){
              this.setState((prevState) => ({
                  ...prevState,
-                 sessionLength: prevState.sessionLength -1
+                 sessionLength: prevState.sessionLength -1,
+                 minutes:prevState.minutes-1
+
              }), () => {
                  // console.log(this.state.sessionLength);
              })
          }
      }
 
-     startStopHandler=()=>{
-         this.setState((prevState)=>({
-             ...prevState,
-            timerStarted:!prevState.timerStarted
-         }))
-     }
+    startStopHandler = () => {
+        let i="";
+        this.setState((prevState) => ({
+            ...prevState,
+            timerStarted: !prevState.timerStarted
+        }), () => {
+            console.log(this.state.timerStarted);
+            if (this.state.timerStarted === true) {
+                let intervalId = setInterval(() => {
+                    if(this.state.seconds==='00'){
+                        this.setState((prevState)=>({
+                            ...prevState,
+                            minutes:prevState.minutes-1,
+                            seconds:60
+                        }))
+                    }
+                    if(this.state.seconds<=10){
+                        i="0";
+                    }
+                    this.setState((prevState) => ({
+                        ...prevState,
+                        seconds: i+(prevState.seconds - 1)
+                    }))
+                    console.log('test');
+                }, 1000);
+
+                console.log(this.state.minutes);
+                this.setState(prevState => ({
+                    ...prevState,
+                    intervalId: intervalId
+                }))
+            }
+            else if (!this.state.timerStarted) {
+                clearInterval(this.state.intervalId);
+            }
+        })
+    }
 
      resetHandler=()=>{
          this.resetFunction();
      }
 
      resetFunction(){
-         this.setState({
-            sessionLength: 25,
-            breakLength: 5,
-            timerStarted:false,
-            timerStatus:"Session"
-         })
+         if(!this.state.timerStarted){
+             this.setState({
+                sessionLength: 25,
+                breakLength: 5,
+                minutes:"25",
+                seconds:"00",
+                timerStarted:false,
+                timerStatus:"Session"
+             })
+         }
      }
 
     render() {
@@ -83,7 +127,7 @@ class Display extends Component {
                     lengthId="session-length" lengthValue={this.state.sessionLength} lengthIncrease={this.incrementHandler} lengthDecrease={this.decrementHandler}
                 />
                 <Timer timerProcessStatus={this.state.timerStarted} 
-                timerStatus={this.state.timerStatus} sessionLength={this.state.sessionLength}/>
+                timerStatus={this.state.timerStatus} sessionLengthMinutes={this.state.minutes} sessionLengthSeconds={this.state.seconds}/>
                 <FunctionBlock startStopClicked={this.startStopHandler} resetClicked={this.resetHandler}/>
             </div>
         )
