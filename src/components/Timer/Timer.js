58721de74e07019282cbbb5fdd83eee437203b9d
Farid Huseynov <faridhuseynov.eng@gmail.com>
2020-12-21 16:09:45 +0400

    CSS set up completely

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -5,7 +5,7 @@ const timer = (props) => {
     let color="white";
     {props.sessionLengthMinutes<1?(color="red"):(color="white")}
     return (
-        <div style={{color:color}}>
+        <div style={{color:color}} className="Timer">
             <p id="timer-label">{props.timerStatus}</p>
             <p id="time-left">{props.sessionLengthMinutes<10?"0":""}{props.sessionLengthMinutes} : {props.sessionLengthSeconds<10?"0":""}{props.sessionLengthSeconds}</p>
         </div>
